7f6a5052ff6e7cbf8698b3c8ab1bc811b11e50f9
GuilhermeKageyama <guiproc@gmail.com>
2022-07-25 11:33:43 -0300

    chakra-ui e render pag + liks dinamicos

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import TopBar from "../components/TopBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <TopBar />
+      <Box maxWidth="container.xl" margin="auto">
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
